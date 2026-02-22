8f9f7f0255b8345940b360740c7809699e950acb
Amanda <amanda.asgm@gmail.com>
2026-02-22 16:39:42 -0300

    feat(navegando): criando forma de navegar atraves de button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,21 @@
+import { useNavigate } from "react-router"
+
 export function Home(){
+  const navigate = useNavigate()
+
+  function goProducts(){
+    navigate("/products")
+  }
+
+
   return (
     <div>
       <h1>Pagina home</h1>
       <nav>
         <a href="/products"> Produtos</a>
         <a href="/products?category=tvs"> Categoria</a>
+
+        <button type="button" onClick={goProducts}>Ver produtos</button>
       </nav>
     </div>
   )
