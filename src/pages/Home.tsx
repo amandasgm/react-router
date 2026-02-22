17c10069bf263fc8c0c0c6ff48108057aa53569a
Amanda <amanda.asgm@gmail.com>
2026-02-22 16:30:13 -0300

    feat(useSearchParams):  adicionando parametros a url, principalmente para filtrar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ export function Home(){
       <h1>Pagina home</h1>
       <nav>
         <a href="/products"> Produtos</a>
+        <a href="/products?category=tvs"> Categoria</a>
       </nav>
     </div>
   )
